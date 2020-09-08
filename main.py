63af0cdb40070eedfafec702985fe79b72dd2df2
Rafal <rafista.stanek@gmail.com>
2020-09-08 14:41:53 +0200

    autopressing button

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import cv2 as cv
 import numpy as np
-import keyboard
 import threading
 import time
+import random
 
 import pyautogui
 from pyautogui import press, typewrite, hotkey
@@ -17,7 +13,6 @@ global isSequence
 isSequence = False
 
 def check_frame(frame):
-    #img_rgb = cv.imread(frame)
     img_rgb = frame
 
     height, width, _ = img_rgb.shape
@@ -26,7 +21,7 @@ def check_frame(frame):
     templates = []
     templ_shapes = []
     threshold = 0.99
-    letters = [] #lista wszystkich liter w sekwencji
+    letters = []
 
     for i in range(letter_count):
         templates.append(cv.imread("src/letter{}.png".format(i+1), 0))
@@ -37,16 +32,12 @@ def check_frame(frame):
         res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
         w, h = template.shape[::-1]
-        #print("***nowy template***")
         for pt in zip(*loc[::-1]):
-            print(pt[0])
             cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 0)
             letters.append([pt[0],a])
         a=a+1
 
     show_sequence(letters)
-    #cv.imwrite('res.png', img_rgb)
-    #print("Zakończono...")
 
 def show_sequence(array):
     array.sort()
@@ -62,6 +53,7 @@ def press_sequence(array):
     print("Wciskam sekwencje")
     global isSequence
     for letter in array:
+        time.sleep(random.uniform(0.3, 0.9))
         if letter[1]==1:
             press("a")
             print("a")
@@ -74,33 +66,23 @@ def press_sequence(array):
         elif letter[1]==4:
             press("s")
             print("s")
-    time.sleep(6.858)
+    waitRandom = random.uniform(8.5, 10.9)
+    print("Klikam Z za sekund: "+str(waitRandom))
+    time.sleep(waitRandom)
+    press("z")
     isSequence=False
 
 def capture_video():
     threading.Timer(1.0, capture_video).start()
     if not isSequence:
         print("Czekam na sekwencje...")
-        SCREEN_SIZE = (1920, 1080)
-        fourcc = cv.VideoWriter_fourcc(*"XVID")
-        # make a screenshot
         img = pyautogui.screenshot()
-        # img = pyautogui.screenshot(region=(0, 0, 300, 400))
-        # convert these pixels to a proper numpy array to work with OpenCV
         frame = np.array(img)
-        # convert colors from BGR to RGB
         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        # write the frame
-        # show the frame
-        # cv.imshow("screenshot", frame)
         check_frame(frame)
-        # if the user clicks q, it exits
-
-        # make sure everything is closed when exited
         cv.destroyAllWindows()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Rozpoczynam działanie programu...')
     capture_video()
